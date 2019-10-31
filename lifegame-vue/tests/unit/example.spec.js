5f431ba29dbeaeb5045f87c6b664ed4543d884e2
maeken2010 <maeken2010@gmail.com>
2019-10-31 23:11:14 +0900

    Add unit test

--- lifegame-vue/tests/unit/example.spec.js
+++ lifegame-vue/tests/unit/example.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
+import Cell from "@/components/Cell.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
@@ -8,5 +9,24 @@ describe("HelloWorld.vue", () => {
       propsData: { msg }
     });
     expect(wrapper.text()).toMatch(msg);
+  });
+});
+
+describe("Cell.vue", () => {
+  it("死んでいるときは生きているClassではない", () => {
+    const state = 0;
+    const wrapper = shallowMount(Cell, {
+      propsData: { state }
+    });
+    expect(wrapper.classes()).toContain("die");
+    expect(wrapper.classes()).not.toContain("live");
+  });
+  it("生きているときは死んでいるClassではない", () => {
+    const state = 1;
+    const wrapper = shallowMount(Cell, {
+      propsData: { state }
+    });
+    expect(wrapper.classes()).toContain("live");
+    expect(wrapper.classes()).not.toContain("die");
   });
 });
