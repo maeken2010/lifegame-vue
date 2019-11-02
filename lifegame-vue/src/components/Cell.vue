73ecb8208224c92cc58e2371192dff257b1e3683
maeken2010 <maeken2010@gmail.com>
2019-11-02 14:11:15 +0900

    add cell components

---
+++ lifegame-vue/src/components/Cell.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="cell" :class="state ? 'alive' : 'dead'">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cell",
+  props: {
+    state: Boolean
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.cell {
+  width: 10px;
+  height: 10px;
+  border: solid 3px #000000;
+}
+
+.alive {
+  color: #000000
+}
+
+.dead {
+  color: #ffffff
+}
+</style>
