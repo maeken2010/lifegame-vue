a795adff33d5cb37a969f73778320b551ebe7280
maeken2010 <maeken2010@gmail.com>
2019-11-02 14:14:03 +0900

    remove hello world

--- lifegame-vue/tests/unit/example.spec.js
+++ lifegame-vue/tests/unit/example.spec.js
@@ -1,16 +1,5 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
 import Cell from "@/components/Cell.vue";
-
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
 
 describe("Cell.vue", () => {
   it("死んでいるときは生きているClassではない", () => {
