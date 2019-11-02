620dcff7c920148fc97b8908537ffbe875d845b8
maeken2010 <maeken2010@gmail.com>
2019-11-02 14:02:55 +0900

    fix class name and state type

--- lifegame-vue/tests/unit/example.spec.js
+++ lifegame-vue/tests/unit/example.spec.js
@@ -14,19 +14,19 @@ describe("HelloWorld.vue", () => {
 
 describe("Cell.vue", () => {
   it("死んでいるときは生きているClassではない", () => {
-    const state = 0;
+    const state = false;
     const wrapper = shallowMount(Cell, {
       propsData: { state }
     });
-    expect(wrapper.classes()).toContain("die");
-    expect(wrapper.classes()).not.toContain("live");
+    expect(wrapper.classes()).toContain("dead");
+    expect(wrapper.classes()).not.toContain("alive");
   });
   it("生きているときは死んでいるClassではない", () => {
-    const state = 1;
+    const state = true;
     const wrapper = shallowMount(Cell, {
       propsData: { state }
     });
-    expect(wrapper.classes()).toContain("live");
-    expect(wrapper.classes()).not.toContain("die");
+    expect(wrapper.classes()).toContain("alive");
+    expect(wrapper.classes()).not.toContain("dead");
   });
 });
