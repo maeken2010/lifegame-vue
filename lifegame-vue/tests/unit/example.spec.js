cc2de392755a039827c2ef71ecc348d81ae5c57c
maeken2010 <maeken2010@gmail.com>
2019-11-02 14:32:36 +0900

    ちょっとDRY

--- lifegame-vue/tests/unit/example.spec.js
+++ lifegame-vue/tests/unit/example.spec.js
@@ -2,19 +2,21 @@ import { shallowMount } from "@vue/test-utils";
 import Cell from "@/components/Cell.vue";
 
 describe("Cell.vue", () => {
+  const factory = ({ props }) => {
+    return shallowMount(Cell, {
+      propsData: props
+    });
+  };
+
   it("死んでいるときは生きているClassではない", () => {
     const state = false;
-    const wrapper = shallowMount(Cell, {
-      propsData: { state }
-    });
+    const wrapper = factory({ props: { state } });
     expect(wrapper.classes()).toContain("dead");
     expect(wrapper.classes()).not.toContain("alive");
   });
   it("生きているときは死んでいるClassではない", () => {
     const state = true;
-    const wrapper = shallowMount(Cell, {
-      propsData: { state }
-    });
+    const wrapper = factory({ props: { state } });
     expect(wrapper.classes()).toContain("alive");
     expect(wrapper.classes()).not.toContain("dead");
   });
